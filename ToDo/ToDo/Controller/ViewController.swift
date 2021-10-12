39c4a3746d51627d44329cc8e11af280f1edde7d
Valeria Fuster <vfustermartins@gmail.com>
2021-10-11 22:59:28 -0300

    create data source and show tasks in tableView

--- ToDo/ToDo/Controller/ViewController.swift
+++ ToDo/ToDo/Controller/ViewController.swift
@@ -7,12 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-
+class ViewController: UIViewController, UITableViewDataSource {
+  
+    private var tasks: [String] = []
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        
+    }
+    
+    // MARK: DATA SOURCE
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task
+        
+        return cell
     }
     
     @IBAction func tapAdd(_ sender: Any) {
@@ -20,8 +40,10 @@ class ViewController: UIViewController {
         alert.addTextField(configurationHandler: nil)
         
         let addAction = UIAlertAction(title: "Adicionar", style: .default) { [unowned alert] _ in
-            let task = alert.textFields![0].text
-            print(task)
+            if let task = alert.textFields![0].text {
+                self.tasks.append(task)
+                self.tableView.reloadData()
+            }
         }
         alert.addAction(addAction)
         
